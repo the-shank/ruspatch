95aacd92a717d6e90e4c3c89496d9c3fe49dc851
wyf <wuyf21@mail.ustc.edu.cn>
2023-05-30 09:35:06 +0800

    after mean

--- costs/data/cal.py
+++ costs/data/cal.py
@@ -2,10 +2,11 @@ import pandas as pd
 
 # read the CSV file
 df = pd.read_csv('e1.csv')
-# df1 = pd.read_csv('e3.csv')
-# df2 = pd.read_csv('e3-r.csv')
 
+# df1 = pd.read_csv('e2.csv')
+# df2 = pd.read_csv('e2-r.csv')
 # df = pd.concat([df1['before'], df2['after']], axis=1)
+
 df['before'] = df['before'] / 1000000
 df['after'] = df['after'] / 1000000
 
@@ -16,6 +17,9 @@ before_q3 = df['before'].quantile(0.75)
 before_siqr = (before_q3 - before_q1) / 2
 
 after_avg = df['after'].mean()
+after_q1 = df['after'].quantile(0.25)
+after_q3 = df['after'].quantile(0.75)
+after_siqr = (after_q3 - after_q1) / 2
 
 increase = (after_avg - before_avg)/before_avg * 100
 
@@ -23,4 +27,5 @@ increase = (after_avg - before_avg)/before_avg * 100
 print('Before - Average:', before_avg)
 print('Before - SIQR:', before_siqr)
 print('After - Average:', after_avg)
+print('After - SIQR:', after_siqr)
 print('Average - Percentage Increase:', increase)
